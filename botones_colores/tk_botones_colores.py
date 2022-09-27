e38d6f7f75f7ac2d2c11166b8c2b643982644817
diegocode <diegocode@gmail.com>
2022-09-27 00:04:18 -0300

    mejoras al código

--- botones_colores/tk_botones_colores.py
+++ botones_colores/tk_botones_colores.py
@@ -1,30 +1,66 @@
 import tkinter
 from tkinter import messagebox, simpledialog
 
+BG_COLOR = '#00CC33'
+FG_COLOR = 'blue'
+BG_COLOR_ALERT = 'red'
+
+BTN_WIDTH = 40
+BTN_WIDTH_ALERT = 30
+BTN_HEIGHT = 4
+BTN_BORDER_WIDTH = 3
+
 mw = tkinter.Tk()
 mw.title("muestra")
-mw.geometry('400x250')
+
+#mw.geometry('400x250')
 
 def calcula():
-    a = simpledialog.askfloat("Número 1", "Ingrese un número entre -10.0 y 10.0", minvalue=-10, maxvalue=10)
-    b = simpledialog.askfloat("Número 2", "Ingrese un número entre -10.0 y 10.0", minvalue=-10, maxvalue=10)
-    messagebox.showinfo("Resultado", str(a + b))
-    
+    flg_cancel_close = False
+    suma = 0
+    for n_numero in 1,2:
+        numero = simpledialog.askfloat(f"Número {n_numero}",
+                                        "Ingrese un número entre -10.0 y 10.0",
+                                       minvalue=-10,
+                                       maxvalue=10)
+        if numero == None:
+            flg_cancel_close = True
+        else:
+            suma += numero
+
+    if not flg_cancel_close:
+        messagebox.showinfo("Resultado", str(suma))
+
+
 def salir():
-    btnSalir.configure(width=20)  # cambia el ancho del botón
-    btnSalir.configure(bg='red')  # cambia el color del boton
-    ans = messagebox.askyesno("Atención", "¿Confirma salir?")
-    if ans == True:
+    btnSalir.configure(width=BTN_WIDTH_ALERT)  # cambia el ancho del botón
+    btnSalir.configure(bg=BG_COLOR_ALERT)  # cambia el color del boton
+    flg_salir = messagebox.askyesno("Atención", "¿Confirma salir?")
+    if flg_salir:
         mw.destroy()  # termina y borra la aplicación de la memoria
     else:
-        btnSalir.configure(width=40)  # restaura ancho original del boton
-        btnSalir.configure(bg='#00CC33')  # restaura color original del boton
+        btnSalir.configure(width=BTN_WIDTH)  # restaura ancho original del boton
+        btnSalir.configure(bg=BG_COLOR)  # restaura color original del boton
 
-btnSalir = tkinter.Button(mw, text="salir", command=salir, width=40, height=4, bd=3, bg='#00CC33',  fg='blue')
-btnCalcular = tkinter.Button(mw, text="calcu", command=calcula, width=40, height=4, bd=3, bg='#00CC33',  fg='blue')
 
-btnSalir.grid(row=0, column=0, padx=20, pady=20)
-btnCalcular.grid(row=1, column=0, padx=20, pady=20)
+btnCalcular = tkinter.Button(mw, text="Suma de dos números",
+                             command=calcula,
+                             width=BTN_WIDTH,
+                             height=BTN_HEIGHT,
+                             bd=BTN_BORDER_WIDTH,
+                             bg=BG_COLOR,
+                             fg=FG_COLOR)
+
+btnSalir = tkinter.Button(mw, text="Salir",
+                          command=salir,
+                          width=BTN_WIDTH,
+                          height=BTN_HEIGHT,
+                          bd=BTN_BORDER_WIDTH,
+                          bg=BG_COLOR,
+                          fg=FG_COLOR)
+
+btnCalcular.grid(row=0, column=0, padx=20, pady=20)
+btnSalir.grid(row=1, column=0, padx=20, pady=20)
 
 mw.mainloop()
 
--- canvas/pie_chart.py
+++ canvas/pie_chart.py
@@ -1,27 +1,31 @@
-def ang(n, t):
-    return 360.0 * n / t
-
 import tkinter
 
-ancho = 512
-alto = 512
-margen = 2
+valores = ((80, 'red'), (300, 'blue'), (150, 'green'), (70, 'orange'))
 
-v1 = 25
-v2 = 25
-v3 = 50
-v4 = 25
-total = v1 + v2 + v3 + v4
+ANCHO = 512
+ALTO = 512
+MARGEN = 20
 
-c = tkinter.Canvas(width=ancho, height=alto);
+def calcula_angulo(n, t):
+    return (360.0 * n) / t
 
-coor = margen, margen, ancho - margen, alto - margen
+mw = tkinter.Tk()
+cnv = tkinter.Canvas(mw, width=ANCHO, height=ALTO);
 
-c.create_arc(coor, fill="red", start=0, extent=ang(v1, total))
-c.create_arc(coor, fill="blue", start=ang(v1, total), extent=ang(v2, total))
-c.create_arc(coor, fill="green", start=ang(v1 + v2, total), extent=ang(v3, total))
-c.create_arc(coor, fill="light sea green", start=ang(v1 + v2+v3, total), extent=ang(v4, total))
+coor = MARGEN, MARGEN, ANCHO - MARGEN, ALTO - MARGEN
 
-c.grid(row=0, column=0)
+total = sum([x[0] for x in valores])
 
-c.mainloop()
+acumulado = 0
+for valor in valores:
+    cnv.create_arc(coor,
+                   fill=valor[1],
+                   outline=valor[1],
+                   start=calcula_angulo(acumulado, total),
+                   extent=calcula_angulo(valor[0], total),
+                   )
+    acumulado += valor[0]
+
+cnv.grid(row=0, column=0, padx=15, pady=15)
+
+mw.mainloop()
