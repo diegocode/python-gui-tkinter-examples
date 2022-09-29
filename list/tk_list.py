0b4181c90c58ff8acd8fcf1f1078a9cf85a73ff7
diegocode <diegocode@g05>
2022-09-28 21:02:10 -0300

    added functionality to tk_listbox

--- list/tk_list.py
+++ list/tk_list.py
@@ -1,6 +1,10 @@
 import tkinter
 from tkinter import messagebox
 
+# callback para doble-click en item
+def ver_item_CB():
+    item_string = lstOpciones.get(lstOpciones.curselection())
+    messagebox.showinfo("item", item_string)
 
 # callback para botón btnBorrar
 def btnBorraCB():
@@ -47,11 +51,25 @@ def btnAgregaCB():
     # coloca el foco en txtItem
     txtItem.focus_set()
 
+def btnLimpiarCB():
+    r = messagebox.askyesno("Atención","¿Confirma borrar?\ntodos los elementos?", default=messagebox.NO)
+
+    if r:
+        # si confirma, borra todos los elementos
+        lstOpciones.delete(0, tkinter.END)
+
+
 top = tkinter.Tk()
 top.title("una lista")
 
+scrVert = tkinter.Scrollbar(top, orient=tkinter.VERTICAL)
+
 # crea Listbox
-lstOpciones = tkinter.Listbox(top)
+lstOpciones = tkinter.Listbox(top, yscrollcommand=scrVert.set)
+
+scrVert.config(command=lstOpciones.yview)
+
+frameBotones = tkinter.Frame(top, bd=1, relief=tkinter.SUNKEN)
 
 # crea Button btnAgregar
 btnAgregar = tkinter.Button(top, text="Agregar", underline=0, command=btnAgregaCB)
@@ -59,24 +77,43 @@ btnAgregar = tkinter.Button(top, text="Agregar", underline=0, command=btnAgregaC
 top.bind('<Alt_L><a>', lambda e:btnAgregaCB())
 
 # crea btnBorrar
-btnBorrar = tkinter.Button(top, text="Borrar", underline=0, command=btnBorraCB)
+btnBorrar = tkinter.Button(frameBotones, text="Borrar", underline=0, command=btnBorraCB)
 # vincula Alt izquierdo + b con función "btnBorraCB"
 top.bind('<Alt_L><b>', lambda e:btnBorraCB())
 
+btnVer = tkinter.Button(frameBotones, text="Ver", underline=0, command=ver_item_CB)
+top.bind('<Alt_L><v>', lambda e:ver_item_CB())
+
+btnLimpiar = tkinter.Button(frameBotones, text="Limpiar", underline=0, command=btnLimpiarCB)
+top.bind('<Alt_L><l>', lambda e:btnLimpiarCB())
+
 # crea btnSalir
-btnSalir = tkinter.Button(top, text="Salir", underline=0, command=btnSaleCB)
+btnSalir = tkinter.Button(frameBotones, text="Salir", underline=0, command=btnSaleCB)
 # vincula Alt izquierdo + s con función "btnSaleCB"
 top.bind('<Alt_L><s>', lambda e:btnSaleCB())
+
 txtItem = tkinter.Entry(top)
 
+
 # posiciona cada widget en columna y fila correspondiente
-btnAgregar.grid(row=0, column=1)
-txtItem.grid(row=0, column=0)
-lstOpciones.grid(row=1, column=0, rowspan=3)
-btnBorrar.grid(row = 2, column = 1, sticky='WES')
-btnSalir.grid(row = 3, column = 1, sticky='WES')
+txtItem.grid(row=0, column=0, columnspan=2, sticky="WE")
+lstOpciones.grid(row=1, column=0, rowspan=4, sticky="WE")
+scrVert.grid(row=1, column=1, rowspan=4, sticky="NS")
+btnAgregar.grid(row=0, column=2, sticky="WE")
+
+frameBotones.grid(row=1, column=2, sticky="NSEW")
+
+btnVer.grid(row=0, column=0, sticky="WE")
+btnBorrar.grid(row=1, column = 0, sticky='WE')
+btnLimpiar.grid(row=2, column=0)
+btnSalir.grid(row=3, column = 0, sticky='WE')
+#frameBotones.grid_propagate(0)
 
 # coloca el foco en txtItem
 txtItem.focus_set()
+
+# agrega algunos elementos a la lista
+for item in range(1, 15):
+    lstOpciones.insert(tkinter.END, item)
 
 top.mainloop()     # queda esperando eventos
---
+++ timer/timercito.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import datetime
+
+SEG_CUENTA = 10
+
+def dar_fecha_hora_iso8601():
+    return datetime.datetime.now().replace(microsecond=0).isoformat()
+
+def descuenta():
+    if segundos.get() >0:
+        segundos.set(segundos.get() - 1)     
+                
+    tiempo.set(dar_fecha_hora_iso8601())
+    mw.after(1000, descuenta)
+
+
+mw = tk.Tk()
+mw.title("timer")
+
+segundos = tk.IntVar()
+segundos.set(SEG_CUENTA)
+lblTiempo = tk.Label(mw, textvar=segundos, font='Arial 36 bold')
+
+tiempo = tk.StringVar()
+tiempo.set(dar_fecha_hora_iso8601())
+lblHora = tk.Label(mw, textvar=tiempo, font='Arial 18 bold')
+
+lblTiempo.grid(row=0, column=0, pady=10)
+lblHora.grid(row=2, column=0, pady=10)
+
+mw.after(1000, descuenta)
+
+mw.mainloop()
